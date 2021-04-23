ee2de3e188dddfc3c96c6c1070416c0dc661fb69
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2021-04-23 18:13:11 +0900

    알고리즘 2016년 풀이

--- AlgorithmTest/AlgorithmTest/Test20210423_1.cs
+++ AlgorithmTest/AlgorithmTest/Test20210423_1.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+// 프로그래머스 코딩 테스트 연습 Level_1
+// 2016년
 
 namespace AlgorithmTest
 {
-    class Test20210423_1
+    public class Test20210423_1
     {
+        public static string solution(int a, int b)
+        {
+            DateTime now = new DateTime(2016, a, b);
+
+            string answer = now.DayOfWeek.ToString();
+
+            answer = answer.Substring(0, 3).ToUpper();
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 24;
+
+            solution(a, b);
+
+            return;
+        }
     }
 }
