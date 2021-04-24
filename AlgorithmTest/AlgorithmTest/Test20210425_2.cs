b9ee2a5ddd0a22f4a64880a855acbc08e12487d2
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2021-04-25 02:42:11 +0900

    알고리즘 음양 더하기, 소수 만들기

---
+++ AlgorithmTest/AlgorithmTest/Test20210425_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 프로그래머스 코딩 테스트 연습 Level_1
+// 음양 더하기
+
+namespace AlgorithmTest
+{
+    public class Test20210425_2
+    {
+        public static int solution(int[] absolutes, bool[] signs)
+        {
+            int count = 0;
+            for(int i = 0; i < absolutes.Length; i++)
+            {
+                if (signs[i])
+                {
+                    count += absolutes[i];
+                }
+                else
+                {
+                    count -= absolutes[i];
+                }
+            }
+
+
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4 };
+            bool[] b = { true, false, false, true };
+
+            solution(a, b);
+
+            return;
+        }
+    }
+}
---
+++ AlgorithmTest/AlgorithmTest/Test20210425_3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 프로그래머스 코딩 테스트 연습 Level_1
+// 소수 만들기
+
+namespace AlgorithmTest
+{
+    public class Test20210425_3
+    {
+        public static int solution(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                for (int j = i + 1; j < nums.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        int num = nums[i] + nums[j] + nums[k];
+
+                        bool check = false;
+
+                        for (int t = 2; t < num; t++)
+                        {
+                            if (num % t == 0)
+                            {
+                                check = true;
+                                break;
+                            }
+                        }
+
+                        if (!check)
+                            count++;
+                    }
+
+                }
+            }
+
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4 };
+
+            solution(a);
+
+            return;
+        }
+    }
+}
