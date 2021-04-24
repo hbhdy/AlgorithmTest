1402748a89a625e5481b8b1d02797ebc46e48a5d
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2021-04-25 02:20:47 +0900

    알고리즘 내적

---
+++ AlgorithmTest/AlgorithmTest/Test20210425_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 프로그래머스 코딩 테스트 연습 Level_1
+// 내적
+
+namespace AlgorithmTest
+{
+    public class Test20210425_1
+    {
+        public static int solution(int[] a, int[] b)
+        {
+            int count = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                count += a[i] * b[i];
+            }
+
+            Console.WriteLine(count);
+
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4 };
+            int[] b = { -3, -1, 0, 2 };
+
+            solution(a, b);
+
+            return;
+        }
+    }
+}
