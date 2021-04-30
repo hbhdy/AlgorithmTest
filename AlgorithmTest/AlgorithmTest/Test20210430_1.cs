f211e95c9e8df73858402e68d0df06ed9b039af3
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2021-04-30 12:50:43 +0900

    알고리즘 1. 자릿수 더하기 2. 자연수 뒤집어 배열로 만들기

---
+++ AlgorithmTest/AlgorithmTest/Test20210430_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 프로그래머스 코딩 테스트 연습 Level_1
+// 자릿수 더하기
+
+namespace AlgorithmTest
+{
+    public class Test20210430_1
+    {
+        public static int solution(int n)
+        {
+            int answer = 0;
+            string str = n.ToString();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                answer += int.Parse(str[i].ToString());
+            }
+
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = 984;
+
+            solution(n);
+
+            return;
+        }
+    }
+}
---
+++ AlgorithmTest/AlgorithmTest/Test20210430_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 프로그래머스 코딩 테스트 연습 Level_1
+// 자연수 뒤집어 배열로 만들기
+
+namespace AlgorithmTest
+{
+    public class Test20210430_2
+    {
+        public static int[] solution(long n)
+        {
+            string str = n.ToString();
+            int[] answer = new int[str.Length];
+
+            int count = 0;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                answer[count] += int.Parse(str[i].ToString());
+                count++;
+            }
+
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            long n = 12345678;
+
+            solution(n);
+
+            return;
+        }
+    }
+}
