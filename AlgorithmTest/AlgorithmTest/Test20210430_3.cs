eea2e4b5a8231f4bd535f1f65ad73591124ff15b
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2021-04-30 22:06:49 +0900

    알고리즘 3. 정수 내림차순으로 배치하기 4. 정수 제곱근 판별

---
+++ AlgorithmTest/AlgorithmTest/Test20210430_3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 프로그래머스 코딩 테스트 연습 Level_1
+// 정수 내림차순으로 배치하기
+
+namespace AlgorithmTest
+{
+    public class Test20210430_3
+    {
+        public static long solution(long n)
+        {
+            long answer = 0;
+            string str = "";
+
+            List<char> temps = new List<char>(n.ToString().ToCharArray());
+
+            temps.Sort();
+            temps.Reverse();
+
+            for (int i = 0; i < temps.Count; i++)
+            {
+                str += temps[i];
+            }
+
+            answer = long.Parse(str.ToString());
+
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            long n = 118372;
+
+            solution(n);
+
+            return;
+        }
+    }
+}
---
+++ AlgorithmTest/AlgorithmTest/Test20210430_4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 프로그래머스 코딩 테스트 연습 Level_1
+// 정수 제곱근 판별
+
+namespace AlgorithmTest
+{
+    public class Test20210430_4
+    {
+        public static long solution(long n)
+        {
+            double d = Math.Sqrt(n);
+
+            if (d % 1 == 0)
+            {
+                return (long)Math.Pow(d + 1, 2);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            long n = 121;
+
+            solution(n);
+
+            return;
+        }
+    }
+}
