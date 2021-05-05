bb58e27ed2d14158610791390482dd13af617ba3
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2021-05-05 18:01:06 +0900

    알고리즘 1. 행렬 테두리 회전하기 2. 소수 찾기

---
+++ AlgorithmTest/AlgorithmTest/Algorithm_Level2/Test20210505_1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+// 프로그래머스 코딩 테스트 연습 Level_2
+// 2021 Dev-Matching: 웹 백엔드 개발자(상반기)
+// 행렬 테투리 회전하기
+
+namespace AlgorithmTest.Algorithm_Level2
+{
+    class Test20210505_1
+    {
+        public static int[] solution(int rows, int columns, int[,] queries)
+        {
+            List<int> nums = new List<int>();
+            Queue<int> queue = new Queue<int>();
+            int[,] cell = new int[rows, columns];
+
+            // 행렬 초기화
+            for (int i = 0; i < cell.GetLength(0); i++)
+            {
+                for (int j = 0; j < cell.GetLength(1); j++)
+                {
+                    cell[i, j] = (i * columns + j + 1);
+                }
+            }
+
+            int min = 0;
+
+            // 큐 삽입
+             for (int i = 0; i < queries.GetLength(0); i++)
+            {
+                // 기본 회전 큐 삽입
+                for (int c = queries[i, 1]; c <= queries[i, 3]; c++)
+                {
+                    queue.Enqueue(cell[queries[i, 0] - 1, c - 1]);
+                }
+
+                for (int c = queries[i, 0]; c <= queries[i, 2]; c++)
+                {
+                    if (!queue.Contains(cell[c - 1, queries[i, 3] - 1]))
+                        queue.Enqueue(cell[c - 1, queries[i, 3] - 1]);
+                }
+
+                for (int c = queries[i, 3]; c >= queries[i, 1]; c--)
+                {
+                    if (!queue.Contains(cell[queries[i, 2] - 1, c - 1]))
+                        queue.Enqueue(cell[queries[i, 2] - 1, c - 1]);
+                }
+
+                for (int c = queries[i, 2]; c >= queries[i, 0]; c--)
+                {
+                    if (!queue.Contains(cell[c - 1, queries[i, 1] - 1]))
+                        queue.Enqueue(cell[c - 1, queries[i, 1] - 1]);
+                }
+
+                //맨앞의 큐값을 최소값으로 설정
+                min = queue.Peek();
+
+                // 회전하는 네모의 왼쪽 상단 구석에서 오른쪽으로 한 칸옆의 자리부터 큐 삽입
+                for (int c = queries[i, 1] + 1; c <= queries[i, 3]; c++)
+                {
+                    if (min > queue.Peek())
+                        min = queue.Peek();
+
+                    cell[queries[i, 0] - 1, c - 1] = queue.Dequeue();
+                }
+
+                for (int c = queries[i, 0] + 1; c <= queries[i, 2]; c++)
+                {
+                    if (min > queue.Peek())
+                        min = queue.Peek();
+
+                    cell[c - 1, queries[i, 3] - 1] = queue.Dequeue();
+                }
+
+                for (int c = queries[i, 3] - 1; c >= queries[i, 1]; c--)
+                {
+                    if (min > queue.Peek())
+                        min = queue.Peek();
+
+                    cell[queries[i, 2] - 1, c - 1] = queue.Dequeue();
+                }
+
+                for (int c = queries[i, 2] - 1; c >= queries[i, 0]; c--)
+                {
+                    if (min > queue.Peek())
+                        min = queue.Peek();
+
+                    cell[c - 1, queries[i, 1] - 1] = queue.Dequeue();
+                }
+
+                // 최소값 추가
+                nums.Add(min);
+            }
+
+            int[] answer = nums.ToArray();
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[,] array = { { 1, 1, 100, 97 } };
+
+            solution(100, 97, array);
+
+            return;
+
+        }
+
+    }
+}
---
+++ AlgorithmTest/AlgorithmTest/Algorithm_Level2/Test20210505_2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 프로그래머스 코딩 테스트 연습 Level_2
+// 소수 찾기
+
+namespace AlgorithmTest.Algorithm_Level2
+{
+    class Test20210505_2
+    {
+        // 한번의 실패후 코드 모두 지움
+        // 다시 시작
+        public static int solution(string numbers)
+        {
+            List<char> chList = new List<char>(numbers.ToCharArray());
+            Dictionary<char, int> dicData = new Dictionary<char, int>();
+
+            // 문자 정렬
+            chList.Sort();
+            chList.Reverse();
+
+            // 문자로 만든 최대값
+            // 각 문자가 몇개인지 계산
+            string str = "";
+            int max = 0;
+            for(int i = 0; i < chList.Count; i++)
+            {
+                if (!dicData.ContainsKey(chList[i]))
+                    dicData.Add(chList[i], 1);
+                else
+                    dicData[chList[i]] += 1;
+
+                str += chList[i].ToString();
+            }
+            max = int.Parse(str.ToString());
+
+
+            //에라토스테네스의 체 적용
+            bool[] check = new bool[max+1];
+            int answer = 0;
+
+            for (int i = 2; i <= max; i++)
+            {
+                if (!check[i])
+                {
+                    // 배수 삭제
+                    for (int j = i * 2; j <= max; j += i)
+                    {
+                        check[j] = true;
+                    }
+                }
+            }
+
+            for (int i = 2; i < check.Length; i++)
+            {
+                if (!check[i])
+                {
+                    bool next = false;
+                    // 얕은 복사 조심
+                    Dictionary<char, int> temp = new Dictionary<char, int>(dicData);
+
+                    for (int j = 0; j < i.ToString().Length; j++)
+                    {
+                        // 해당 소수가 numbers의 개별 문자를 포함하는가?
+                        if (temp.ContainsKey(i.ToString()[j]))
+                        {
+                            if (temp[i.ToString()[j]] != 0)
+                                temp[i.ToString()[j]] -= 1;
+                            else
+                            {
+                                // 부족하면 패스
+                                next = true;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            // 없으면 패스
+                            next = true;
+                            break;
+                        }
+                    }
+
+                    if (next)
+                        continue;
+
+                    answer++;
+                }
+            }
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            string str = "17";
+
+            solution(str);
+
+            return;
+
+        }
+    }
+}
