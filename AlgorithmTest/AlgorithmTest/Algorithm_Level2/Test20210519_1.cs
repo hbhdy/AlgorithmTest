a7932daec64707634879d1fc82c449bef14c917d
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2021-05-19 13:51:00 +0900

    알고리즘 1. 스킬트리 2. 방문 길이

---
+++ AlgorithmTest/AlgorithmTest/Algorithm_Level2/Test20210519_1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프로그래머스 코딩 테스트 연습 Level_2
+// 스킬트리
+
+namespace AlgorithmTest.Algorithm_Level2
+{
+    class Test20210519_1
+    {
+        public static int solution(string skill, string[] skill_trees)
+        {
+            int answer = 0;
+            int treeLength = skill_trees.Length;
+
+            // 단어 수 만큼 for문 
+            for(int i = 0; i < treeLength; i++)
+            {
+                bool check = true;
+                char[] str = skill_trees[i].ToCharArray();
+                int count = 0;
+
+                for(int j = 0; j < str.Length; j++)
+                {
+                    // 해당 첫 문자를 통과하지못하면 컷
+                    if (count < skill.IndexOf(str[j]))
+                    {
+                        Console.WriteLine(skill.IndexOf(str[j]));
+                        check = false;
+                        break;
+                    }
+                    else if (count == skill.IndexOf(str[j]))
+                    {
+                        count++;
+                    }
+                }
+
+                if (check)
+                    answer++;
+            }
+
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            string skill = "CBD";
+            string[] trees = { "BACDE", "CBADF", "AECB", "BDA" };
+
+            solution(skill, trees);
+
+            return;
+        }
+    }
+}
---
+++ AlgorithmTest/AlgorithmTest/Algorithm_Level2/Test20210519_2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프로그래머스 코딩 테스트 연습 Level_2
+// 방문 길이
+
+namespace AlgorithmTest.Algorithm_Level2
+{
+    class Test20210519_2
+    {
+        public static int solution(string dirs)
+        {
+            int x = 0;
+            int y = 0;
+            List<string> data = new List<string>();
+
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                bool check = false;
+                StringBuilder builder = new StringBuilder();
+                int xpos = x;
+                int ypos = y;
+
+                switch (dirs[i])
+                {
+                    case 'U':
+                        y++;
+                        if (y > 5)
+                        {
+                            check = true;
+                            y = 5;
+                        }
+                        break;
+                    case 'D':
+                        y--;
+                        if (y < -5)
+                        {
+                            check = true;
+                            y = -5;
+                        }
+                        break;
+                    case 'R':
+                        x++;
+                        if (x > 5)
+                        {
+                            check = true;
+                            x = 5;
+                        }
+                        break;
+                    case 'L':
+                        x--;
+                        if (x < -5)
+                        {
+                            check = true;
+                            x = -5;
+                        }
+                        break;
+                }
+                if (!check)
+                {
+                    xpos = x + xpos;
+                    ypos = y + ypos;
+
+                    builder.Append(xpos.ToString() + ypos.ToString());
+
+                    if (!data.Contains(builder.ToString()))
+                        data.Add(builder.ToString());
+                }
+            }
+
+            //data = data.Distinct().ToList();
+
+            return data.Count;
+        }
+
+        public static void Main(string[] args)
+        {
+            string dir = "LULLLLLLU";
+
+            solution(dir);
+
+            return;
+        }
+    }
+}
