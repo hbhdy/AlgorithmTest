5bf67e19799c1ee03f1de2da4351e233ee476f1f
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2021-05-23 19:04:19 +0900

    알고리즘 1. 다음 큰 숫자 2. JadenCase 문자열 만들기

---
+++ AlgorithmTest/AlgorithmTest/Algorithm_Level2/Test20210523_1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프로그래머스 코딩 테스트 연습 Level_2
+// 다음 큰 숫자
+
+namespace AlgorithmTest.Algorithm_Level2
+{
+    class Test20210523_1
+    {
+        public static int solution(int n)
+        {
+            int answer = 0;
+            int num = 0;
+
+            string temp = Convert.ToString(n, 2);
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] == '1')
+                    num++;
+            }
+
+            n++;
+            while(true)
+            {
+                int count = 0;
+                string str = Convert.ToString(n, 2);
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] == '1')
+                        count++;
+                }
+
+                if (num == count)
+                {
+                    answer = n;
+                    break;
+                }
+                else
+                    n++;
+            }
+
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = 78;
+
+            solution(n);
+
+            return;
+        }
+    }
+}
---
+++ AlgorithmTest/AlgorithmTest/Algorithm_Level2/Test20210523_2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프로그래머스 코딩 테스트 연습 Level_2
+// JadenCase 문자열 만들기
+
+namespace AlgorithmTest.Algorithm_Level2
+{
+    class Test20210523_2
+    {
+        public static string solution(string s)
+        {
+            s = s.ToLower();
+
+            char[] ch = s.ToCharArray();
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (i == 0)
+                {
+                    builder.Append(ch[i].ToString().ToUpper());
+                }
+                else
+                {
+                    if (ch[i - 1] == ' ')
+                    {
+                        builder.Append(ch[i].ToString().ToUpper());
+                    }
+                    else
+                    {
+                        builder.Append(ch[i]);
+                    }
+                }
+            }
+
+            string answer = builder.ToString();
+            return answer;
+        }
+
+        public static void Main(string[] args)
+        {
+            string s = "3people unFollowed me";
+
+            solution(s);
+
+            return;
+        }
+    }
+}
